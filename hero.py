0ac742d00f9b605db5f6bd89aff8a2cd129a0e6e
Konstantinos Maravegias <kwstas.maras@gmail.com>
2020-09-15 20:35:51 +0100

    fixed a bug in the self.health of Hero class

--- hero.py
+++ hero.py
@@ -3,38 +3,39 @@ import pygame
 BLACK = (0,0,0)
 
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, color, height, width, health):
+    def __init__(self, color, height, width, hero_health):
         super().__init__()
+
 
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
+
         # Draw the spaceship
         pygame.draw.rect(self.image, color, [0, 0, width, height])
+
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
-        self.health = health
+        self.health = hero_health
 
     def moveUp(self, pixels):
         self.rect.y -= pixels
-		# Check if the Hero(spacehip) is off the edge of the screen (y-axis)
+		# Check if the paddles are off the edge of the screen (y-axis)
         if self.rect.y < 590:
             self.rect.y = 590
 
     def moveDown(self, pixels):
         self.rect.y += pixels
-        # Check if the Hero is off the edge of the screen (y-axis)
-        if self.rect.y > 730:
-            self.rect.y = 730
+        # Check if the paddles are off the edge of the screen (y-axis)
+        if self.rect.y > 740:
+            self.rect.y = 740
 
     def moveLeft(self, pixels):
-        # Check if the Hero is off the edge of the screen (y-axis)
         self.rect.x -= pixels
-        if self.rect.x < 0:
-            self.rect.x = 0
+        if self.rect.x < 8:
+            self.rect.x = 8
 
     def moveRight(self, pixels):
         self.rect.x += pixels
-        # Check if the Hero is off the edge of the screen (y-axis)
-        if self.rect.x > 690:
-            self.rect.x = 690
+        if self.rect.x > 670:
+            self.rect.x = 670
